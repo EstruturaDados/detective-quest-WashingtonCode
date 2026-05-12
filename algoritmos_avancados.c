b92f24ce8aa40a2f1b2ebac85ca944480dc3cd0f
WashingtonCode <202501483143@alunos.unifanor.edu.br>
2026-05-12 21:04:08 +0000

    Inclusão das bibliotecas necessárias

--- algoritmos_avancados.c
+++ algoritmos_avancados.c
@@ -1,3 +1,7 @@
 // Desafio: Detective Quest - Nível Novato
 // Objetivo: Desenvolver um jogo onde você aplicará a árvore binária para criar o mapa interativo da mansão em Detective Quest. Implemente funções criarSala(), explorarSalas() e navegue do Hall de entrada até um nó-folha, garantindo usabilidade, legibilidade e documentação clara.
 
+// Inclusão das bibliotecas necessárias
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
