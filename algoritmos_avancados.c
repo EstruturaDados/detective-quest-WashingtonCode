9fb51421ef056449d8c08784b56a0c360a237c79
WashingtonCode <202501483143@alunos.unifanor.edu.br>
2026-05-12 22:48:29 +0000

    Inclusão das bibliotecas necessárias

--- algoritmos_avancados.c
+++ algoritmos_avancados.c
@@ -1,3 +1,7 @@
 // Desafio: Detective Quest - Nível Mestre
 // Objetivo: Desenvolver a integração da árvore binária do mapa, BST de pistas e tabela hash de suspeitos, conduzindo a exploração interativa da mansão, coleta automática de pistas e acusação final. Seu programa confirmará evidências e encerrará a investigação em C.
 
+// Inclusão das bibliotecas necessárias
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
